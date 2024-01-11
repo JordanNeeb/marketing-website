411d19f7a673d87ecc1c61973e3bcc2c75305d1e
JordanNeeb <jordanscottneeb@gmail.com>
2024-01-10 21:42:26 -0330

    adds 404

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 
@@ -36,6 +38,7 @@ const Hero = () => {
                 <button
                   type="button"
                   className="inline-flex items-center gap-x-2 rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+                  onClick={() => window.print()}
                 >
                   <ArrowDownTrayIcon
                     className="-ml-0.5 h-5 w-5"
