b631614e120c36171293dbabaeaac98c60d39109
JordanNeeb <140026335+JordanNeeb@users.noreply.github.com>
2024-02-14 15:34:56 -0330

    saves show docs to local storage

--- app/chat/page.js
+++ app/chat/page.js
@@ -1,12 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Docs from "../components/chat/Docs";
 import Chat from "../components/chat/Chat";
 
 const ChatPage = () => {
-  const [showDocs, setShowDocs] = useState(true);
+  const [showDocs, setShowDocs] = useState(
+    JSON.parse(localStorage.getItem("jnShowDocs"))
+  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const jnShowDocs = localStorage.getItem("jnShowDocs");
+
+    if (jnShowDocs === "null") {
+      setShowDocs(true);
+    } else {
+      setShowDocs(JSON.parse(jnShowDocs));
+    }
+
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("jnShowDocs", JSON.stringify(showDocs));
+  }, [showDocs]);
+
+  if (loading) {
+    return <div className="h-screen bg-white" />;
+  }
 
   return (
     <div className="min-h-screen bg-white">
